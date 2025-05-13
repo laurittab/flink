b2fad028b6ee758869eb6acdccc30a76cc54e315
Rita Burrows <rita.burrows@cloudworks.cc>
2025-05-13 21:35:46 +0100

    somple flink app

--- quickstart/src/main/java/org/myorg/quickstart/DataStreamJob.java
+++ quickstart/src/main/java/org/myorg/quickstart/DataStreamJob.java
@@ -1,65 +1,34 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.myorg.quickstart;
 
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * Skeleton for a Flink DataStream Job.
- *
- * <p>For a tutorial how to write a Flink application, check the
- * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ * A simple stateless Flink DataStream Job.
+ * It generates a sequence of numbers, filters even numbers, and prints them.
  */
 public class DataStreamJob {
 
-	public static void main(String[] args) throws Exception {
-		// Sets up the execution environment, which is the main entry point
-		// to building Flink applications.
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    public static void main(String[] args) throws Exception {
+        // Sets up the execution environment for the Flink application
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.fromSequence(1, 10);
-		 *
-		 * then, transform the resulting DataStream<Long> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.window()
-		 * 	.process()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide:
-		 *
-		 * https://nightlies.apache.org/flink/flink-docs-stable/
-		 *
-		 */
+        // Create a DataStream (a sequence of numbers in this case)
+        DataStream<Long> numbers = env.fromSequence(1, 1000000000);
 
-		// Execute program, beginning computation.
-		env.execute("Flink Java API Skeleton");
-	}
+        // Apply a simple transformation: filter even numbers
+        DataStream<Long> filteredNumbers = numbers.filter(new FilterFunction<Long>() {
+            @Override
+            public boolean filter(Long value) throws Exception {
+                return value % 2 == 0; // Only allow even numbers
+            }
+        });
+
+        // Print the filtered result to stdout
+        filteredNumbers.print("Filtered Even Numbers");
+
+        // Execute the job, which starts the data processing
+        env.execute("Simple Stateless Flink Job");
+    }
 }
